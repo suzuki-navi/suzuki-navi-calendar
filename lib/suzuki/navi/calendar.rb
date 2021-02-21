591285ed0d1e910bbe3c9186a66053c78505869a
suzuki-navi <suzuki.navi.dev@gmail.com>
2021-02-21 12:39:55 +0900

    dummy tool

--- lib/suzuki/navi/calendar.rb
+++ lib/suzuki/navi/calendar.rb
@@ -5,8 +5,11 @@ require_relative "calendar/version"
 module Suzuki
   module Navi
     module Calendar
-      class Error < StandardError; end
-      # Your code goes here...
+      def main
+        puts("Hello")
+        p ARGV
+      end
+      module_function :main
     end
   end
 end
